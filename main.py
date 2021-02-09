89446b884608c1dc79badf942b149db51b75eaa6
slava <mister.slava1508@yandex.ru>
2021-02-09 18:31:19 +0300

    c

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import pygame
+import requests
+
+
+d, s, m = float(
+    input(
+
+    )
+), float(
+    input(
+
+    )
+), float(
+    input(
+
+    )
+)
+map_request = f"http://static-maps.yandex.ru/1.x/?ll={d},{s}&spn={m},{m}&l=map"
+response = requests.get(map_request)
+
+if not response:
+    print("Ошибка выполнения запроса:")
+    print(map_request)
+    print("Http статус:", response.status_code, "(", response.reason, ")")
+    sys.exit(1)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+
+os.remove(map_file)
