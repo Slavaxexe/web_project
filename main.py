b8cfa623d1b72fa7bdf63801f9a36d7c02a320a9
slava <mister.slava1508@yandex.ru>
2021-02-09 20:14:21 +0300

    esssss

--- main.py
+++ main.py
@@ -22,12 +22,20 @@ pygame.init()
 screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
+mashtab = 0.005
+new_m = m
 running = True
 while running:
     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
         if event.type == pygame.KEYDOWN:
             last_s = s
             last_d = d
+            if event.key == pygame.K_PAGEUP:
+                new_m = m - mashtab
+            if event.key == pygame.K_PAGEDOWN:
+                new_m = m + mashtab
             if event.key == pygame.K_LEFT:
                 d -= m / 3
             if event.key == pygame.K_RIGHT:
@@ -36,20 +44,17 @@ while running:
                 s += m / 3
             if event.key == pygame.K_DOWN:
                 s -= m / 3
-            map_request = f"http://static-maps.yandex.ru/1.x/?ll={d},{s}&spn={m},{m}&l=map"
+            map_request = f"http://static-maps.yandex.ru/1.x/?ll={d},{s}&spn={new_m},{new_m}&l=map"
             response = requests.get(map_request)
+            map_file = "map.png"
             if not response:
-                s = last_s
-                d = last_d
-                print("Ошибка выполнения запроса:")
+                print('Ошибка выполения запроса')
             else:
-                map_file = "map.png"
+                m = new_m
                 with open(map_file, "wb") as file:
                     file.write(response.content)
-                screen.blit(pygame.image.load(map_file), (0, 0))
-        elif event.type == pygame.QUIT:
-            running = False
+    screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
-
+    screen.fill((0, 0, 0))
 pygame.quit()
 os.remove(map_file)
