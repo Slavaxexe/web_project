7c65ec0e330c2fa8a9e9b2d8640f514e463746cb
ЯЛицей <kol@yandex.ru>
2021-02-09 19:19:37 +0300

    merge

--- main.py
+++ main.py
@@ -23,10 +23,25 @@ screen = pygame.display.set_mode((600, 450))
 screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
 running = True
-
-
-
-
+while running:
+    for event in pygame.event.get():
+        if event.key != None:
+            if event.key == pygame.K_LEFT:
+                d -= m / 5
+            if event.key == pygame.K_RIGHT:
+                d -= m / 5
+            if event.key == pygame.K_UP:
+                s += m / 5
+            if event.key == pygame.K_DOWN:
+                s -= m / 5
+            map_request = f"http://static-maps.yandex.ru/1.x/?ll={d},{s}&spn={m},{m}&l=map"
+            response = requests.get(map_request)
+            map_file = "map.png"
+            with open(map_file, "wb") as file:
+                file.write(response.content)
+            screen.blit(pygame.image.load(map_file), (0, 0))
+        if event.type == pygame.QUIT:
+            running = False
 
 pygame.quit()
 os.remove(map_file)
