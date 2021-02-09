2794b231a8eb3c559c4bf581b9c22874d580b884
ЯЛицей <kol@yandex.ru>
2021-02-09 19:48:59 +0300

    merge

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
+            last_s = s
+            last_d = d
             if event.key == pygame.K_LEFT:
                 d -= m / 3
             if event.key == pygame.K_RIGHT:
@@ -36,15 +38,15 @@ while running:
                 s -= m / 3
             map_request = f"http://static-maps.yandex.ru/1.x/?ll={d},{s}&spn={m},{m}&l=map"
             response = requests.get(map_request)
-            map_file = "map.png"
-            with open(map_file, "wb") as file:
-                file.write(response.content)
             if not response:
+                s = last_s
+                d = last_d
                 print("Ошибка выполнения запроса:")
-                print(map_request)
-                print("Http статус:", response.status_code, "(", response.reason, ")")
-                sys.exit(1)
-            screen.blit(pygame.image.load(map_file), (0, 0))
+            else:
+                map_file = "map.png"
+                with open(map_file, "wb") as file:
+                    file.write(response.content)
+                screen.blit(pygame.image.load(map_file), (0, 0))
         elif event.type == pygame.QUIT:
             running = False
     pygame.display.flip()
