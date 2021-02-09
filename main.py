9e661daa65edf7b6c43215b9f4bfebedcc941d33
ЯЛицей <kol@yandex.ru>
2021-02-09 18:51:08 +0300

    c

--- main.py
+++ main.py
@@ -25,8 +25,21 @@ pygame.display.flip()
 running = True
 while running:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        if event.key != None:
+            if event.key == pygame.K_LEFT:
+                d -= m / 5
+            if event.key == pygame.K_RIGHT:
+                d -= m / 5
+            if event.key == pygame.K_UP:
+                s += m / 5
+            if event.key == pygame.K_DOWN:
+                s -= m / 5
+            map_request = f"http://static-maps.yandex.ru/1.x/?ll={d},{s}&spn={m},{m}&l=map"
+            response = requests.get(map_request)
+            map_file = "map.png"
+            with open(map_file, "wb") as file:
+                file.write(response.content)
+            screen.blit(pygame.image.load(map_file), (0, 0))
 
 pygame.quit()
 os.remove(map_file)
