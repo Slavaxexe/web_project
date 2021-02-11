0cec42f28283f9739f9b2d1c37ec8f92c700862d
Xelopat <61018159+Xelopat@users.noreply.github.com>
2021-02-11 20:27:57 +0300

    7 task

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from time import time
+
 import pygame
 import requests
 
@@ -6,15 +8,15 @@ from Button import Button
 
 
 # reload map
-def reload(m_1, d_1, s_1, map_type, pts):
+def reload(m_1, d_1, s_1, map_type, points):
     # add points to map
     all_pt = ""
-    if (pts != []):
+    if points:
         all_pt = "&pt="
-        for i in range(len(pts)):
+        for i in range(len(points)):
             if i != 0:
                 all_pt += "~"
-            all_pt += pts[i][0] + "," + pts[i][1] + ",round"
+            all_pt += points[i][0] + "," + points[i][1] + ",round"
     # request for new map
     map_req = f"http://static-maps.yandex.ru/1.x/?ll={d_1},{s_1}&spn={m_1},{m_1}&l={map_type}{all_pt}"
     resp = requests.get(map_req)
@@ -28,30 +30,41 @@ def reload(m_1, d_1, s_1, map_type, pts):
 
 # find place by name
 def find_place(text_1):
-    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&geocode={text_1}&format=json"
-    response = requests.get(geocoder_request)
-    if not response:
-        print("Ошибка выполнения запроса")
-        return 0, 0
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-98533de7710b&" \
+                       f"geocode={text_1}&format=json"
+    response_1 = requests.get(geocoder_request)
+    if not response_1:
+        print("Ошибка выполнения запроса3")
+        return 0, 0, "Null"
     else:
         try:
-            cords = (
-                response.json()["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"][
-                    "Point"][
-                    "pos"])
+            response_json = response_1.json()
+            cords = response_json["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"][
+                "Point"][
+                "pos"]
+            place = response_json["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"][
+                "metaDataProperty"][
+                "GeocoderMetaData"]["text"]
         except IndexError:
-            print("Ошибка выполнения запроса")
-            return 0, 0
-    return float(cords.split()[0]), float(cords.split()[1])
+            print("Ошибка выполнения запроса2")
+            return 0, 0, "Null"
+    return float(cords.split()[0]), float(cords.split()[1]), place
 
 
+# Type of the map
 now_type = "map"
+# All points
 pts = []
+# Can we write&
 active = False
+# Scale
 m = float(input())
+# Last time for scrolling
+last_time = time()
+now_sim = 0
 pygame.init()
 
-screen = pygame.display.set_mode((600, 500))
+screen = pygame.display.set_mode((600, 550))
 
 # UI - text
 input_box = pygame.Rect(300, 0, 200, 50)
@@ -59,22 +72,27 @@ pygame.draw.rect(screen, (0, 0, 0), input_box, 1)
 font = pygame.font.Font(None, 24)
 text = "Moscow"
 txt_surface = font.render(text, True, (0, 0, 0))
-screen.blit(txt_surface, (input_box.x + 20, input_box.y + 20))
-find = Button(500, 0, 100, 50, (255, 255, 255), "Enter", (0, 0, 0), 20)
+input_box_2 = pygame.Rect(300, 50, 200, 50)
+pygame.draw.rect(screen, (0, 0, 0), input_box_2, 1)
+text_2 = ""
+text_place = font.render(text_2, True, (0, 0, 0))
+text_2_enter = text_2
 # UI - Buttons
-schem = Button(0, 0, 100, 50, (150, 150, 150), "Схема", (0, 0, 0), 20)
-sattalite = Button(100, 0, 100, 50, (255, 255, 255), "Спутник", (0, 0, 0), 20)
+find = Button(500, 0, 100, 50, (255, 255, 255), "Enter", (0, 0, 0), 20)
+remove = Button(500, 50, 100, 50, (255, 255, 255), "Remove", (0, 0, 0), 20)
+schema = Button(0, 0, 100, 50, (150, 150, 150), "Схема", (0, 0, 0), 20)
+satellite = Button(100, 0, 100, 50, (255, 255, 255), "Спутник", (0, 0, 0), 20)
 hybrid = Button(200, 0, 100, 50, (255, 255, 255), "Гибрид", (0, 0, 0), 20)
 pygame.display.flip()
 running = True
 # First request
 search = "Moscow"
-d, s = find_place(search)
+d, s, _ = find_place(search)
 map_request = f"http://static-maps.yandex.ru/1.x/?ll={d},{s}&spn={m},{m}&l=map"
 response = requests.get(map_request)
 map_file = "map.png"
 if not response:
-    print("Ошибка выполнения запроса")
+    print("Ошибка выполнения запроса1")
 else:
     with open(map_file, "wb") as file:
         file.write(response.content)
@@ -91,28 +109,35 @@ while running:
             else:
                 active = False
             # Set mode schema
-            if schem.pressed(event.pos):
-                schem.change_color((150, 150, 150), screen)
-                sattalite.change_color((255, 255, 255), screen)
+            if schema.pressed(event.pos):
+                schema.change_color((150, 150, 150), screen)
+                satellite.change_color((255, 255, 255), screen)
                 hybrid.change_color((255, 255, 255), screen)
                 now_type = "map"
-            # Set mode sattalite
-            elif sattalite.pressed(event.pos):
-                schem.change_color((255, 255, 255), screen)
-                sattalite.change_color((150, 150, 150), screen)
+            # Set mode satellite
+            elif satellite.pressed(event.pos):
+                schema.change_color((255, 255, 255), screen)
+                satellite.change_color((150, 150, 150), screen)
                 hybrid.change_color((255, 255, 255), screen)
                 now_type = "sat"
             # Set mode hybrid
             elif hybrid.pressed(event.pos):
                 hybrid.change_color((150, 150, 150), screen)
-                sattalite.change_color((255, 255, 255), screen)
-                schem.change_color((255, 255, 255), screen)
+                satellite.change_color((255, 255, 255), screen)
+                schema.change_color((255, 255, 255), screen)
                 now_type = "sat,skl"
             # Find new place and make a point
             elif find.pressed(event.pos):
-                d, s = find_place(text)
-                if d != 0 and s != 0:
+                d, s, text_2 = find_place(text)
+                text_2 = "     " + text_2
+                text_2_enter = text_2
+                now_sim = 0
+                if d != 0 and s != 0 and [str(d), str(s)] not in pts:
                     pts.append([str(d), str(s)])
+            elif remove.pressed(event.pos):
+                pts = []
+                text_2 = ""
+                text_2_enter = ""
             # Reload if not tap on change-text window
             if not input_box.collidepoint(event.pos):
                 reload(m, d, s, now_type, pts)
@@ -123,8 +148,11 @@ while running:
             if active:
                 # Enter a value, add new point
                 if event.key == pygame.K_RETURN:
-                    d, s = find_place(text)
-                    if d != 0 and s != 0:
+                    d, s, text_2 = find_place(text)
+                    text_2 = "     " + text_2
+                    text_2_enter = text_2
+                    now_sim = 0
+                    if d != 0 and s != 0 and [str(d), str(s)] not in pts:
                         pts.append([str(d), str(s)])
                     active = False
                 # Remove last symbol
@@ -156,19 +184,31 @@ while running:
                     s = last_s
                     m = last_m
                     d = last_d
+    #show_all_text
+    if time() - last_time > 0.1 and len(text_2) > 19:
+        last_time = time()
+        if now_sim != len(text_2):
+            now_sim += 1
+        else:
+            now_sim = 0
+        text_2_enter = text_2[now_sim:now_sim + 19]
     # Fill screen
     screen.fill((255, 255, 255))
     # Show a map
-    screen.blit(pygame.image.load(map_file), (0, 50))
+    screen.blit(pygame.image.load(map_file), (0, 100))
     # Draw buttons
+    remove.draw_button(screen)
     find.draw_button(screen)
-    schem.draw_button(screen)
-    sattalite.draw_button(screen)
+    schema.draw_button(screen)
+    satellite.draw_button(screen)
     hybrid.draw_button(screen)
     # Reload input text
     pygame.draw.rect(screen, (0, 0, 0), input_box, 1)
     txt_surface = font.render(text, True, (0, 0, 0))
     screen.blit(txt_surface, (input_box.x + 20, input_box.y + 20))
+    pygame.draw.rect(screen, (0, 0, 0), input_box_2, 1)
+    text_place = font.render(text_2_enter, True, (0, 0, 0))
+    screen.blit(text_place, (input_box_2.x + 10, input_box_2.y + 20))
     # Yeah flippy-bottle challenge!!!
     pygame.display.flip()
 pygame.quit()
