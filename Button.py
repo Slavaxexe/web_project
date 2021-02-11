29d4728ec7f29e9a5b95c4790958a456339c8ef2
Xelopat <61018159+Xelopat@users.noreply.github.com>
2021-02-11 16:11:14 +0300

    commit

---
+++ Button.py
@@ -0,0 +1,33 @@
+import pygame
+
+
+class Button:
+    def __init__(self, x, y, size_x, size_y, color, text, text_color, text_size):
+        self.x = x
+        self.y = y
+        self.size_x = size_x
+        self.size_y = size_y
+        self.text = text
+        self.color = color
+        self.text_color = text_color
+        self.text_size = text_size
+
+    def draw_button(self, screen):
+        pygame.draw.rect(screen, self.color, (self.x, self.y, self.size_x, self.size_y))
+        self.write_text(screen)
+
+    def write_text(self, surface):
+        font_size = self.text_size
+        myFont = pygame.font.Font(None, 24)
+        myText = myFont.render(self.text, 1, self.text_color)
+        surface.blit(myText, ((self.x + self.size_x / 2) - myText.get_width() / 2,
+                              (self.y + self.size_y / 2) - myText.get_height() / 2))
+
+    def pressed(self, mouse):
+        if self.x + self.size_x > mouse[0] > self.x  and self.y + self.size_y > mouse[1] > self.y:
+            return True
+        return False
+
+    def change_color(self, color, screen):
+        self.color = color
+        self.draw_button(screen)
--- main.py
+++ main.py
@@ -1,32 +1,69 @@
 import os
-import sys
 import pygame
 import requests
 
+from Button import Button
+
+
+def reload(m_1, d_1, s_1, map_type):
+    map_req = f"http://static-maps.yandex.ru/1.x/?ll={d_1},{s_1}&spn={m_1},{m_1}&l={map_type}"
+    resp = requests.get(map_req)
+    map_f = "map.png"
+    if not resp:
+        return False
+    else:
+        with open(map_f, "wb") as file_1:
+            file_1.write(resp.content)
+        screen.blit(pygame.image.load(map_file), (0, 50))
+        return True
+
 
 d, s, m = float(input()), float(input()), float(input())
+now_type = "map"
+
 map_request = f"http://static-maps.yandex.ru/1.x/?ll={d},{s}&spn={m},{m}&l=map"
 response = requests.get(map_request)
-
 if not response:
     print("Ошибка выполнения запроса:")
-    print(map_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
-
 map_file = "map.png"
 with open(map_file, "wb") as file:
     file.write(response.content)
 
+
 pygame.init()
-screen = pygame.display.set_mode((600, 450))
-screen.blit(pygame.image.load(map_file), (0, 0))
+
+screen = pygame.display.set_mode((600, 500))
+screen.fill((255, 255, 255))
+screen.blit(pygame.image.load(map_file), (0, 50))
+schem = Button(0, 0, 100, 50, (150, 150, 150), "Схема", (0, 0, 0), 20)
+sattalite = Button(100, 0, 100, 50, (255, 0, 255), "Спутник", (0, 0, 0), 20)
+hybride = Button(200, 0, 100, 50, (0, 0, 255), "Гибрид", (0, 0, 0), 20)
+schem.draw_button(screen)
+sattalite.draw_button(screen)
+hybride.draw_button(screen)
 pygame.display.flip()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if schem.pressed(event.pos):
+                schem.change_color((150, 150, 150), screen)
+                sattalite.change_color((255, 0, 255), screen)
+                hybride.change_color((0, 0, 255), screen)
+                now_type = "map"
+            elif sattalite.pressed(event.pos):
+                schem.change_color((0, 255, 0), screen)
+                sattalite.change_color((150, 150, 150), screen)
+                hybride.change_color((0, 0, 255), screen)
+                now_type = "sat"
+            elif hybride.pressed(event.pos):
+                hybride.change_color((150, 150, 150), screen)
+                sattalite.change_color((255, 0, 255), screen)
+                schem.change_color((0, 255, 0), screen)
+                now_type = "sat,skl"
+            reload(m, d, s, now_type)
         if event.type == pygame.KEYDOWN:
             last_s = s
             last_d = d
@@ -34,7 +71,7 @@ while running:
             if event.key == pygame.K_PAGEUP:
                 m *= 2
             if event.key == pygame.K_PAGEDOWN:
-                m /=  2
+                m /= 2
             if event.key == pygame.K_LEFT:
                 d -= m / 3
             if event.key == pygame.K_RIGHT:
@@ -43,18 +80,10 @@ while running:
                 s += m / 3
             if event.key == pygame.K_DOWN:
                 s -= m / 3
-            map_request = f"http://static-maps.yandex.ru/1.x/?ll={d},{s}&spn={m},{m}&l=map"
-            response = requests.get(map_request)
-            map_file = "map.png"
-            if not response:
+            if not(reload(m, d, s, now_type)):
+                s = last_s
                 m = last_m
                 d = last_d
-                s = last_s
-                print('Ошибка выполения запроса')
-            else:
-                with open(map_file, "wb") as file:
-                    file.write(response.content)
-                screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
 pygame.quit()
 os.remove(map_file)
